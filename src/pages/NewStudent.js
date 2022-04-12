502366a4b0cf81d8764712c7dc6cef5397ca5f71
Felix Enrique Vega Nolasco <49852681+FelixVNolasco@users.noreply.github.com>
2022-04-12 16:25:02 -0500

    handleInputChange to forms

--- src/pages/NewStudent.js
+++ src/pages/NewStudent.js
@@ -2,8 +2,23 @@ import React from "react";
 import { Navbar } from "../Components/Navbar";
 import { Sidebar } from "../Components/Sidebar";
 import { Footer } from "../Components/Footer";
+import { useForm } from "../hooks/useForm";
 
 const NewStudent = () => {
+  const [formValues, handleInputChange] = useForm({
+    boleta: "2019602194",
+    nombre: "Felix Enrique",
+    apellido: "Vega Nolasco",
+    correo: "felixvnolasco@gmail.com",
+    carrera: "Ingeniería en Informática",
+    plantel: "",
+    inscrito: "",
+  });
+
+  console.log(formValues);
+  const { boleta, nombre, apellido, correo, carrera, plantel, inscrito } =
+    formValues;
+
   return (
     <>
       <Navbar />
@@ -20,50 +35,82 @@ const NewStudent = () => {
                 <span className="w-24 font-semibold">Boleta</span>
                 <input
                   className="w-full ml-4 p-2 border-2 border-green-500 focus:outline-none focus:border-2 focus:border-green-700 rounded-md"
+                  name="boleta"
                   type="text"
+                  onChange={handleInputChange}
+                  value={boleta}
                 />
               </div>
               <div className="flex p-6 items-center">
                 <span className="w-24 font-semibold">Nombre(s)</span>
                 <input
                   className="w-full ml-4 p-2 border-2 border-green-500 focus:outline-none focus:border-2 focus:border-green-700 rounded-md"
+                  name="nombre"
                   type="text"
+                  onChange={handleInputChange}
+                  value={nombre}
                 />
               </div>
               <div className="flex p-6 items-center">
                 <span className="w-24 font-semibold">Apellidos</span>
                 <input
                   className="w-full ml-4 p-2 border-2 border-green-500 focus:outline-none focus:border-2 focus:border-green-700 rounded-md"
+                  name="apellido"
                   type="text"
+                  onChange={handleInputChange}
+                  value={apellido}
                 />
               </div>
               <div className="flex p-6 items-center">
                 <span className="w-24 font-semibold">Correo</span>
                 <input
                   className="w-full ml-4 p-2 border-2 border-green-500 focus:outline-none focus:border-2 focus:border-green-700 rounded-md"
+                  name="correo"
                   type="text"
+                  onChange={handleInputChange}
+                  value={correo}
+                />
+              </div>
+              <div className="flex p-6 items-center">
+                <span className="w-24 font-semibold">Carrera</span>
+                <input
+                  className="w-full ml-4 p-2 border-2 border-green-500 focus:outline-none focus:border-2 focus:border-green-700 rounded-md"
+                  name="carrera"
+                  type="text"
+                  onChange={handleInputChange}
+                  value={carrera}
                 />
               </div>
               <div className="flex p-6 items-center">
                 <span className="w-24 font-semibold">Plantel</span>
                 <select
                   className="w-full ml-4 p-2 border-2 border-green-500 focus:outline-none focus:border-2 focus:border-green-700 rounded-md"
-                  name=""
+                  name="plantel"
                   id=""
+                  onChange={handleInputChange}
+                  value={plantel}
                 >
-                  <option value="UPIICSA">UPIICSA</option>
+                  <option disabled>Seleccione una opción</option>
+                  <option value="UPIICSA" defaultChecked>
+                    UPIICSA
+                  </option>
                   <option value="ESIME">ESIME</option>
                   <option value="UPIITA">UPIITA</option>
                   <option value="ESCOM">ESCOM</option>
                 </select>
               </div>
               <div className="flex p-6 items-center">
-                <span className="w-24 font-semibold">Actualmente esta inscrito?</span>
+                <span className="w-24 font-semibold">
+                  Actualmente esta inscrito?
+                </span>
                 <select
                   className="w-full ml-4 p-2 border-2 border-green-500 focus:outline-none focus:border-2 focus:border-green-700 rounded-md"
-                  name=""
+                  name="inscrito"
                   id=""
+                  onChange={handleInputChange}
+                  value={inscrito}
                 >
+                  <option disabled>Seleccione una opción</option>
                   <option value="true">Si</option>
                   <option value="false">No</option>
                 </select>
--- src/pages/StudentPage.js
+++ src/pages/StudentPage.js
@@ -2,27 +2,38 @@ import React, { useEffect, useState } from "react";
 import { Navbar } from "../Components/Navbar";
 import { Sidebar } from "../Components/Sidebar";
 import { Footer } from "../Components/Footer";
-import { useLocation } from "react-router-dom";
-import axios from "axios";
+// import { useLocation } from "react-router-dom";
+// import axios from "axios";
+import { useForm } from "../hooks/useForm";
 
 const StudentPage = () => {
-  const location = useLocation();
-  const studentId = location.pathname.split("/")[2];
-  const [product, setProduct] = useState({});
+  // const location = useLocation();
+  // const studentId = location.pathname.split("/")[2];
+  // const [product, setProduct] = useState({});
+  const [modifying, setModifying] = useState(false);
 
-  useEffect(() => {
-    const getProduct = async () => {
-      try {
-        const product = await axios.get(
-          `https://us-east-1.aws.data.mongodb-api.com/app/olympus-oocpc/endpoint/api/products/find?id=${studentId}`
-        );
-        setProduct(product.data);
-      } catch (error) {
-        console.log("Error en obtener alumno");
-      }
-    };
-    getProduct();
-  }, [studentId]);
+  const [formValues, handleInputChange] = useForm({
+    boleta: "",
+    nombre: "",
+    apellidos: "",
+    correo: "",
+  });
+
+  const { boleta, nombre, apellidos, correo } = formValues;
+
+  // useEffect(() => {
+  //   const getProduct = async () => {
+  //     try {
+  //       const product = await axios.get(
+  //         // `https://us-east-1.aws.data.mongodb-api.com/app/olympus-oocpc/endpoint/api/products/find?id=${studentId}`
+  //       );
+  //       setProduct(product.data);
+  //     } catch (error) {
+  //       console.log("Error en obtener alumno");
+  //     }
+  //   };
+  //   getProduct();
+  // }, [studentId]);
 
   const estaincrito = true;
   return (
@@ -34,50 +45,145 @@ const StudentPage = () => {
         <div className="flex flex-col mt-2 mr-6 w-5/6 h-screen border-slate-400 border-2 rounded-lg drop-shadow-lg shadow-sm shadow-slate-500 p-10">
           <div className="flex flex-col p-6 w-full items-center">
             <div className="flex flex-col p-24 bg-gradient-to-r from-gray-100 via-gray-100/50 to-gray-200 border-2 border-slate-200 rounded-md">
-              <div className="flex text-xl">
-                <span className="w-24 font-semibold">Boleta: </span>
-                <span className="font-semibold ml-2">2019602194</span>
-              </div>
+              {modifying ? (
+                <>
+                  <div className="animate__animated animate__fadeIn animate__faster">
+                    <h4 className="text-2xl font-bold mb-4 text-center">
+                      Modificar Datos
+                    </h4>
+                    <div className="mb-4 items-center flex text-xl ">
+                      <span className="w-24 font-semibold">Boleta: </span>
+                      <input
+                        className="ml-2 p-2 rounded-md border-2 border-green-500 focus:outline-none focus:border-2 focus:border-green-700"
+                        type="text"
+                        name="boleta"
+                        value={boleta}
+                        onChange={handleInputChange}
+                      />
+                    </div>
 
-              <div className="flex text-xl">
-                <span className="w-24 font-semibold">Nombre: </span>
-                <span className="font-semibold ml-2">Felix Enrique</span>
-              </div>
+                    <div className="mb-4 items-center flex text-xl ">
+                      <span className="w-24 font-semibold">Nombre: </span>
+                      <input
+                        className="ml-2 p-2 rounded-md border-2 border-green-500 focus:outline-none focus:border-2 focus:border-green-700"
+                        type="text"
+                        name="nombre"
+                        value={nombre}
+                        onChange={handleInputChange}
+                      />
+                    </div>
 
-              <div className="flex text-xl">
-                <span className="w-24 font-semibold">Apellidos: </span>
-                <span className="font-semibold ml-2">Vega Nolasco</span>
-              </div>
+                    <div className="mb-4 items-center flex text-xl ">
+                      <span className="w-24 font-semibold">Apellidos: </span>
+                      <input
+                        className="ml-2 p-2 rounded-md border-2 border-green-500 focus:outline-none focus:border-2 focus:border-green-700"
+                        type="text"
+                        name="apellidos"
+                        value={apellidos}
+                        onChange={handleInputChange}
+                      />
+                    </div>
 
-              <div className="flex text-xl">
-                <span className="w-16 font-semibold">Correo: </span>
-                <span className="font-semibold ml-10">felixvnolasco@gmail.com</span>
-              </div>
+                    <div className="mb-4 items-center flex text-xl ">
+                      <span className="w-16 font-semibold">Correo: </span>
+                      <input
+                        className="ml-10 p-2 rounded-md border-2 border-green-500 focus:outline-none focus:border-2 focus:border-green-700"
+                        type="text"
+                        name="correo"
+                        value={correo}
+                        onChange={handleInputChange}
+                      />
+                    </div>
 
-              <div className="flex text-xl">
-                <span className="w-16 font-semibold">Plantel: </span>
-                <span className="font-semibold ml-10">UPIICSA</span>
-              </div>
+                    <div className="flex justify-end p-2">
+                      <div
+                        className="p-4 text-center font-semibold items-center bg-red-400 hover:bg-red-500 hover:bg-green rounded-md cursor-pointer"
+                        onClick={() => setModifying(!modifying)}
+                      >
+                        Cancelar
+                      </div>
+                      <div
+                        className="ml-6 p-4 text-center font-semibold items-center bg-green-400 hover:bg-green-500 hover:bg-green rounded-md cursor-pointer"
+                        onClick={() => console.log("confirmar")}
+                      >
+                        Confirmar
+                      </div>
+                    </div>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <div className="animate__animated animate__fadeInDown animate__faster">
+                    <h4 className="text-2xl font-bold mb-4 text-center">
+                      Datos del alumno
+                    </h4>
+                    <div className="flex text-xl">
+                      <span className="w-24 font-semibold">Boleta: </span>
+                      <span className="font-semibold ml-2 mb-4">
+                        2019602194
+                      </span>
+                    </div>
 
-              <div className="flex text-xl items-center mt-6 mb-6">
-                <p className="font-semibold">Actualmente esta inscrito?</p>
-                <div
-                  className={
-                    !estaincrito
-                      ? "ml-6 h-4 w-4 bg-red-400 rounded-full"
-                      : "ml-6 h-4 w-4 bg-green-400 rounded-full"
-                  }
-                ></div>
-              </div>
+                    <div className="flex text-xl">
+                      <span className="w-24 font-semibold">Nombre: </span>
+                      <span className="font-semibold ml-2 mb-4">
+                        Felix Enrique
+                      </span>
+                    </div>
 
-              <div className="flex justify-end p-2">
-                <div className="p-4 w-24 text-center font-semibold items-center bg-yellow-400 hover:bg-yellow-500 hover:bg-green rounded-md cursor-pointer">
-                  Modificar
-                </div>
-                <div className="ml-6 p-4 w-24 text-center font-semibold items-center bg-red-400 hover:bg-red-500 hover:bg-green rounded-md cursor-pointer">
-                  Eliminar
-                </div>
-              </div>
+                    <div className="flex text-xl">
+                      <span className="w-24 font-semibold">Apellidos: </span>
+                      <span className="font-semibold ml-2 mb-4">
+                        Vega Nolasco
+                      </span>
+                    </div>
+
+                    <div className="flex text-xl">
+                      <span className="w-16 font-semibold">Carrera: </span>
+                      <span className="font-semibold ml-10 mb-4">
+                        Ingeniería en Informática
+                      </span>
+                    </div>
+
+                    <div className="flex text-xl">
+                      <span className="w-16 font-semibold">Plantel: </span>
+                      <span className="font-semibold ml-10 mb-4">UPIICSA</span>
+                    </div>
+
+                    <div className="flex text-xl">
+                      <span className="w-16 font-semibold">Correo: </span>
+                      <span className="font-semibold ml-10 mb-4">
+                        felixvnolasco@gmail.com
+                      </span>
+                    </div>
+
+                    <div className="flex text-xl items-center mt-4 mb-6">
+                      <p className="font-semibold">
+                        Actualmente esta inscrito?
+                      </p>
+                      <div
+                        className={
+                          !estaincrito
+                            ? "ml-6 h-4 w-4 bg-red-400 rounded-full"
+                            : "ml-6 h-4 w-4 bg-green-400 rounded-full"
+                        }
+                      ></div>
+                    </div>
+
+                    <div className="flex justify-end p-2">
+                      <div
+                        className="p-4 w-24 text-center font-semibold items-center bg-yellow-400 hover:bg-yellow-500 hover:bg-green rounded-md cursor-pointer"
+                        onClick={() => setModifying(!modifying)}
+                      >
+                        Modificar
+                      </div>
+                      <div className="ml-6 p-4 w-24 text-center font-semibold items-center bg-red-400 hover:bg-red-500 hover:bg-green rounded-md cursor-pointer">
+                        Eliminar
+                      </div>
+                    </div>
+                  </div>
+                </>
+              )}
             </div>
           </div>
         </div>
